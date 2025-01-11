cf025cd5e51ed0943dd1a0fb90a53c3ab46e32b9
Matt Naples <mattnaples50@gmail.com>
2025-01-11 06:48:58 -0500

    fix wherefield

--- Sources/FirestoreService/FirestoreServices/Query+Extensions.swift
+++ Sources/FirestoreService/FirestoreServices/Query+Extensions.swift
@@ -21,20 +21,22 @@ public extension Query {
     }
     
     /// will include all entries that are ≥ start and ≤ end (inclusive both ends)
-    func whereFieldIsBetween(_ field: String, startDate: Date, endDate: Date) -> Query{
-        let startComponents = Calendar.current.dateComponents([.year, .month, .day], from: startDate)
-        let endComponents = Calendar.current.dateComponents([.year, .month, .day], from: endDate)
-        guard
-            let start = Calendar.current.date(from: startComponents),
-            let end = Calendar.current.date(from: endComponents)
-        else {
-            fatalError("Could not find start date or calculate end date.")
+    func whereFieldIsBetween(_ field: String, startDate: Date, endDate: Date) -> Query {
+        let calendar = Calendar.current
+
+        // Get start date truncated to the beginning of the day
+         let start = calendar.startOfDay(for: startDate)
+
+        // Get end date as midnight of the next day
+        guard let end = calendar.date(byAdding: .day, value: 1, to: calendar.startOfDay(for: endDate)) else {
+            fatalError("Could not calculate end date.")
         }
-//        print("start: \(start)")
-//        print("end: \(end)")
-        
-        return self.whereField(field, isGreaterThanOrEqualTo: start).whereField(field, isLessThanOrEqualTo: end)
+
+        return self
+            .whereField(field, isGreaterThanOrEqualTo: start)
+            .whereField(field, isLessThan: end)
     }
+
     func whereFieldIsOnOrAfter(_ field: String, date: Date) -> Query{
         let components = Calendar.current.dateComponents([.year, .month, .day], from: date)
 
